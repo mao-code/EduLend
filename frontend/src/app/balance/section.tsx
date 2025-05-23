4471d765ee15f9fde5520f05a76a94d5c19dccbb
HsinYing0315 <wusinging0315@gmail.com>
2025-05-24 03:37:51 +0800

    feat: add BalanceSection component for account balance display

---
+++ frontend/src/app/balance/section.tsx
@@ -0,0 +1,23 @@
+'use client'
+import { useAccountStore } from "@/providers/account-store-provider"
+import { useShallow } from "zustand/shallow";
+import FuncButton from './button';
+import type { FuncButtonType } from "./utils";
+
+export default function BalanceSection() {
+    const { balance } = useAccountStore(useShallow((state) => ({
+        balance: state.account.balance,
+    })));
+    const btnList: FuncButtonType[] = ["deposit", "borrow", "repay", "redeem"];
+
+    return (
+        <div className="flex flex-col grow gap-12 w-full items-center justify-center">
+            <h1 className="text-4xl font-bold">Balance: {balance}</h1>
+            <div className="flex gap-8">
+                {btnList.map((btn) => (
+                    <FuncButton key={btn} type={btn} />
+                ))}
+            </div>
+        </div>
+    );
+}
