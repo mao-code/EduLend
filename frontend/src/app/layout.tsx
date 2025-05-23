e01911d6f3786e42666d945feab1caa39f1ce987
HsinYing0315 <wusinging0315@gmail.com>
2025-05-24 03:22:47 +0800

    style: adjust layout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -28,11 +28,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased h-screen flex flex-col`}
       >
         <AccountStoreProvider>
           <Header />
-          {children}
+          <main className="flex-1">{children}</main>
         </AccountStoreProvider>
       </body>
     </html>
