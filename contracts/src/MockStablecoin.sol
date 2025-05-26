33ff6affac276881605645bcbb2a4231fcde75b3
litrar <ninghsuan1818@gmail.com>
2025-05-27 00:56:23 +0800

    [fix] adjust boding curve to achieve liquidation event

--- contracts/src/MockStablecoin.sol
+++ contracts/src/MockStablecoin.sol
@@ -5,7 +5,7 @@ import "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 import "openzeppelin-contracts/contracts/access/Ownable.sol";
 
 contract MockStablecoin is ERC20, Ownable {
-    constructor() ERC20("MockUSDT", "mUSDT") {}
+    constructor() ERC20("MockUSDT", "mUSDT") Ownable(msg.sender) {}
 
     function mint(address to, uint256 amount) external onlyOwner {
         _mint(to, amount);
--- contracts/src/PriceOracle.sol
+++ contracts/src/PriceOracle.sol
@@ -5,8 +5,18 @@ contract PriceOracle {
     uint256[] public prices;
     uint256 public idx;
 
-    constructor(uint256[] memory _trajectory) {
-        prices = _trajectory;
+    /// @notice 使用經 x*y=k bonding curve 計算出的價格軌跡，單位為 mUSDT/EDU
+    constructor() {
+        prices = new uint256[](5);
+
+        // 假設 1 ETH = 2000 mUSDT，將 ETH 價格換算為 mUSDT 價格
+        // 前 4 天價格 ≥ 80%，第 5 天跌破 80%（以 0.001 ETH = 2.0 mUSDT 作基準）
+        prices[0] = 2000000000000000000;  // 2.0000 mUSDT
+        prices[1] = 1900000000000000000;  // 1.9000 mUSDT
+        prices[2] = 1800000000000000000;  // 1.8000 mUSDT
+        prices[3] = 1700000000000000000;  // 1.7000 mUSDT
+        prices[4] = 1599999999999999999;  // 1.6000 mUSDT (剛好 < 80%)
+
         idx = 0;
     }
 
