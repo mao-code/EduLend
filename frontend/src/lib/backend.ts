f4081889017b08a28b0bc3e817987cd7c953586d
HsinYing0315 <wusinging0315@gmail.com>
2025-06-06 01:00:33 +0800

    feat: add backend module with contract ABIs and Ethereum URL

---
+++ frontend/src/lib/backend.ts
@@ -0,0 +1,6 @@
+import lendingPlatformContractABI from '../abi/LendingPlatform-abi.json';
+import lendingPlatformContractAddr from '../abi/LendingPlatform-addr.json';
+import eduTokenContractABI from '../abi/EduToken-abi.json';
+
+export { lendingPlatformContractABI, lendingPlatformContractAddr, eduTokenContractABI };
+export const ETHEREUM_URL = 'http://127.0.0.1:8545';
