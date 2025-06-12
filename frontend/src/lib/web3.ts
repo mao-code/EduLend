f78ebbe231deb85bcd6b68ad4dd7933c91f084fc
HsinYing0315 <wusinging0315@gmail.com>
2025-06-12 20:13:59 +0800

    refactor: rename backend -> web3

---
+++ frontend/src/lib/web3.ts
@@ -0,0 +1,8 @@
+import lendingPlatformContractABI from '../abi/LendingPlatform-abi.json';
+import lendingPlatformContractAddr from '../abi/LendingPlatform-addr.json';
+import eduTokenContractABI from '../abi/EduToken-abi.json';
+import priceOracleContractABI from '../abi/PriceOracle-abi.json';
+import priceOracleContractAddr from '../abi/PriceOracle-addr.json';
+
+export { lendingPlatformContractABI, lendingPlatformContractAddr, eduTokenContractABI, priceOracleContractABI, priceOracleContractAddr };
+export const ETHEREUM_URL = 'http://127.0.0.1:8545';
